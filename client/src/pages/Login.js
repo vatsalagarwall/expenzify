86778fcacdbffd8430a636562ce18ba4912a8991
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-07-05 00:57:25 +0530

    commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,115 +1,61 @@
-// import React, { useState, useEffect } from 'react'
-// import { Link, useNavigate } from 'react-router-dom'
-// import { Form, Input, message } from 'antd';
-// import axios from 'axios'
-// import Spinner from '../components/Spinner';
-
-// const Login = () => {
-//     const navigate = useNavigate();
-//     const [loading, setLoading] = useState(false)
-//     //form submit
-//     const submitHandler = async (values) => {
-//         // console.log(values);
-//         try {
-//             setLoading(true)
-//             const { data } = await axios.post('/users/login', values)
-//             setLoading(false)
-//             message.success('Login Successful')
-//             localStorage.setItem('user', JSON.stringify({ ...data.user, password: '' }))
-//             navigate('/')
-//         }
-//         catch (error) {
-//             setLoading(false)
-//             message.error('Something went wrong')
-//         }
-//     }
-
-//     //prevent to login user
-//     useEffect(() => {
-//         if (localStorage.getItem("user")) {
-//             navigate("/")
-//         }
-//     }, [navigate])
-//     return (
-//         <>
-//             <div className='register-page'>
-//                 {loading && <Spinner />}
-//                 <Form layout='vertical' onFinish={submitHandler}>
-//                     <h1>Login Form</h1>
-//                     <Form.Item label="Email" name="email">
-//                         <Input type='email' />
-//                     </Form.Item>
-//                     <Form.Item label="Password" name="password">
-//                         <Input type='password' />
-//                     </Form.Item>
-//                     <div className='d-flex'>
-//                         <Link to="/register">Not registered ? Click here to register</Link>
-//                         <button className='logout-button'>Login</button>
-//                     </div>
-
-//                 </Form>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Login
-
-
-
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Form, Input, Button, message } from 'antd';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Form, Input, message } from 'antd';
+import axios from 'axios'
 import Spinner from '../components/Spinner';
-import './Login.css'; // Import your CSS file for component-specific styles
 
 const Login = () => {
     const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-
+    const [loading, setLoading] = useState(false)
+    //form submit
     const submitHandler = async (values) => {
+        // console.log(values);
         try {
-            setLoading(true);
-            const { data } = await axios.post('/users/login', values);
-            setLoading(false);
-            message.success('Login Successful');
-            localStorage.setItem('user', JSON.stringify({ ...data.user, password: '' }));
-            navigate('/');
-        } catch (error) {
-            setLoading(false);
-            message.error('Something went wrong');
+            setLoading(true)
+            const { data } = await axios.post('/users/login', values)
+            setLoading(false)
+            message.success('Login Successful')
+            localStorage.setItem('user', JSON.stringify({ ...data.user, password: '' }))
+            navigate('/')
         }
-    };
+        catch (error) {
+            setLoading(false)
+            message.error('Something went wrong')
+        }
+    }
 
+    //prevent to login user
     useEffect(() => {
-        if (localStorage.getItem('user')) {
-            navigate('/');
+        if (localStorage.getItem("user")) {
+            navigate("/")
         }
-    }, [navigate]);
-
+    }, [navigate])
     return (
-        <div className='form-container'>
-            {loading && <Spinner />}
-            <div className='logo-container'>Login Form</div>
-            <Form layout='vertical' onFinish={submitHandler}>
-                <Form.Item label='Email' name='email'>
-                    <Input type='email' className='form-input' />
-                </Form.Item>
-                <Form.Item label='Password' name='password'>
-                    <Input type='password' className='form-input' />
-                </Form.Item>
-                <Form.Item>
-                    <Button type='primary' htmlType='submit' className='form-submit-btn'>
-                        Login
-                    </Button>
-                </Form.Item>
-                <div className='signup-link'>
-                    Not registered? <Link to='/register' className='link'>Sign up now</Link>
-                </div>
-            </Form>
-        </div>
-    );
-};
+        <>
+            <div className='register-page'>
+                {loading && <Spinner />}
+                <Form layout='vertical' onFinish={submitHandler}>
+                    <h1>Login Form</h1>
+                    <Form.Item label="Email" name="email">
+                        <Input type='email' />
+                    </Form.Item>
+                    <Form.Item label="Password" name="password">
+                        <Input type='password' />
+                    </Form.Item>
+                    <div className='d-flex'>
+                        <Link to="/register">Not registered ? Click here to register</Link>
+                        <button className='logout-button'>Login</button>
+                    </div>
 
-export default Login;
+                </Form>
+            </div>
+        </>
+    )
+}
+
+export default Login
+
+
+
+
+
